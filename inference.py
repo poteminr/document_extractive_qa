483be5779bc20c9add43e04a550f2e5c3cdfeef9
Roman Potemin <poteminr@gmail.com>
2023-04-21 23:52:30 +0700

    create script for creating predictions.json

--- inference.py
+++ inference.py
@@ -1,12 +1,33 @@
 import torch
+import pandas as pd
+from tqdm import tqdm
 from transformers import pipeline, AutoModelForQuestionAnswering
 from models import BaselineModel
 from utils.options import train_options
+from utils.set_seed import seed_everything
 
+
+def create_prediction(test_dataframe, qa_pipepline, path_to_save='predictions.json'):
+    extracted_part = []
+    for i in tqdm(range(len(test_dataframe))):
+        row = test_dataframe.iloc[i]
+        question, context = row['label'], row['text']
+        prediction = qa_pipepline(question=question, context=context)
+        row_extracted_part = {
+            'text': [prediction['answer']],
+            'answer_start': [prediction['start']],
+            'answer_end': [prediction['end']]
+        }
+        extracted_part.append(row_extracted_part)
+        
+    test_dataframe['extracted_part'] = extracted_part
+    test_dataframe.to_json(path_to_save)
 
 
 if __name__ == '__main__':
     arguments = train_options()  
+    seed_everything(1007)
+    test_dataframe = pd.read_json(arguments.test_file_path)
     
     if arguments.model_type == 'huggingface':
         model = AutoModelForQuestionAnswering.from_pretrained(arguments.encoder_model)
@@ -21,3 +42,4 @@ if __name__ == '__main__':
 
     if arguments.model_type == 'huggingface':
         qa_pipepline = pipeline("question-answering", model=model, tokenizer=arguments.encoder_model)
+        create_prediction(test_dataframe, qa_pipepline)
---
+++ utils/set_seed.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+import random
+import torch
+from transformers import set_seed
+
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    set_seed(seed)
+    
