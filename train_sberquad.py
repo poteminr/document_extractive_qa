c2e4e34958af8c5a385492001f3fdf478161e65c
Roman Potemin <poteminr@gmail.com>
2023-04-24 00:31:55 +0700

    add sberquad dataset for fine-tuning model

---
+++ train_sberquad.py
@@ -0,0 +1,32 @@
+import wandb
+from transformers import AutoModelForQuestionAnswering
+from models import BaselineModel
+from utils.dataset import get_train_val_sberquad
+from utils.options import train_options
+from utils.set_seed import seed_everything
+from trainer import TrainerConfig, Trainer
+
+
+def train_config_to_dict(train_config: TrainerConfig):
+    return dict((name, getattr(train_config, name)) for name in dir(train_config) if not name.startswith('__'))
+
+
+if __name__ == "__main__":
+    arguments = train_options()  
+    seed_everything(arguments.seed)
+    train_dataset, val_dataset = get_train_val_sberquad()
+    
+    if arguments.model_type == 'huggingface':
+        model = AutoModelForQuestionAnswering.from_pretrained(arguments.encoder_model)
+    else:        
+        model = BaselineModel(encoder_model=arguments.encoder_model)
+
+    config = TrainerConfig()
+    config.model_type = arguments.model_type
+    config.train_label = arguments.train_label
+    config.encoder_model = arguments.encoder_model
+    config.seed = arguments.seed
+    
+    wandb.init(project="Kontur", config=train_config_to_dict(config))
+    trainer = Trainer(model=model, config=config, train_dataset=train_dataset, val_dataset=val_dataset)
+    trainer.train()
--- utils/dataset.py
+++ utils/dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 import torch
 from transformers import AutoTokenizer
 from sklearn.model_selection import train_test_split
-
+from datasets import load_dataset
     
 def get_train_val_dataset(file_path: str, test_size: float = 0.25, max_instances: int = -1, train_label: int = -1):
     if train_label == 1:
@@ -22,7 +22,14 @@ def get_train_val_dataset(file_path: str, test_size: float = 0.25, max_instances
     train_dataset = DocumentDataset(train_dataframe)
     val_dataset = DocumentDataset(val_dataframe)
     return train_dataset, val_dataset
-    
+
+
+def get_train_val_sberquad(test_size: float = 0.2):
+    dataset = load_dataset('sberquad', split='validation').train_test_split(test_size=test_size)
+    train, test = dataset['train'], dataset['test']   
+    train_dataset, test_dataset = SberquadDataset(train), SberquadDataset(test)
+    return train_dataset, test_dataset
+
 
 class DocumentDataset(torch.utils.data.Dataset):
     def __init__(self, dataframe: pd.DataFrame, encoder_model: str = 'cointegrated/rubert-tiny2'):
@@ -75,4 +82,52 @@ class DocumentDataset(torch.utils.data.Dataset):
     def get_answer(self, sample_id: int):
         answer = self.dataframe[self.dataframe.id == sample_id].extracted_part.iloc[0]
         return {'text': answer['text'], 'answer_start':answer['answer_start']}
+
+
+class SberquadDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, encoder_model: str = 'cointegrated/rubert-tiny2'):
+        self.dataset = dataset.filter(lambda x: x["answers"]['answer_start'] != [-1])
+        self.encoder_model = encoder_model
+        self.tokenizer = AutoTokenizer.from_pretrained(self.encoder_model)
+        self.tokenized_input = self.build_dataset()
+                
+    def __getitem__(self, idx):
+        return {key: torch.tensor(val[idx]) for key, val in self.tokenized_input.items()}
+
+    def __len__(self):
+        return len(self.tokenized_input.input_ids)
+    
+    def add_token_positions(self, tokenized_input, answers):
+        start_positions = []
+        end_positions = []
+        for i in range(len(answers)):
+            answer_start = answers[i]['answer_start'][0]
+            answer_end = answer_start + len(answers[i]['text'][0])
+            if answer_start == answer_end:
+                # the answer is not in the context
+                start_positions.append(0)
+                end_positions.append(0)
+            else:    
+                start_positions.append(tokenized_input.char_to_token(i, answer_start, sequence_index=1))
+                end_positions.append(tokenized_input.char_to_token(i, answer_end - 1, sequence_index=1))
+            # if None, the answer passage has been truncated
+            if start_positions[-1] is None:
+                start_positions[-1] = self.tokenizer.model_max_length
+            if end_positions[-1] is None:
+                end_positions[-1] = self.tokenizer.model_max_length
+        tokenized_input.update({'start_positions': start_positions, 'end_positions': end_positions})
         
+    def build_dataset(self):
+        ids, contexts, questions, answers = self.dataset['id'], self.dataset['context'], self.dataset['question'], self.dataset['answers']
+        tokenized_input = self.tokenizer(questions, contexts, truncation=True, padding=True, return_offsets_mapping=True)
+        self.add_token_positions(tokenized_input, answers)
+        tokenized_input.update({'id':ids})
+        return tokenized_input
+    
+    def get_context(self, sample_id: int):
+        # return self.dataframe[self.dataframe.id == sample_id].text.iloc[0]
+        return self.dataset.filter(lambda x: x["id"] == sample_id)['context'][0]
+    
+    def get_answer(self, sample_id: int):
+        answer = self.dataset.filter(lambda x: x["id"] == sample_id)['answers'][0]
+        return {'text': answer['text'], 'answer_start':answer['answer_start']}
